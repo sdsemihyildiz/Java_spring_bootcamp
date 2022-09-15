59ca9a256f73a83d55f85958532f078a0df7b857
SDSemoch <sdsemihyildiz@gmail.com>
2022-09-15 16:42:23 +0300

    up

---
+++ Intro/src/MükemmelSayı/Main.java
@@ -0,0 +1,21 @@
+package MükemmelSayı;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 34;
+        int total = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+
+            }
+        }
+        if (total == number) {
+            System.out.println("Mükemmel sayıdır");
+        } else {
+            System.out.println("Mükemmel sayı değildir");
+        }
+
+    }
+}
--- Intro/src/StringsDemo/Main.java
+++ Intro/src/StringsDemo/Main.java
@@ -1,5 +1,7 @@
 package StringsDemo;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,8 +11,8 @@ public class Main {
    /*
         System.out.println("Element number: " + message.length());
 
-        System.out.println("5. element: " + message.charAt(4));
-        System.out.println(message.concat(" Yeey!"));
+        System.out.println("5. element: " + message.charAt(4)); // istediğimiz indexteki elemanı getirme
+        System.out.println(message.concat(" Yeey!")); // mesajı başka bir mesaj ile birleştirme
         System.out.println(message.startsWith("T"));
         System.out.println(message.endsWith("."));
         char [] c = new char[5];
@@ -21,7 +23,20 @@ public class Main {
         System.out.println(message.lastIndexOf("we"));
 
     */
-        System.out.println(message.replace(' ','-'));
+        String newMessage = message.replace(' ', '-'); //Boşlukları - işareti ile değiştiriyor
+        System.out.println("New Message is: " + newMessage);
+        System.out.println(message.substring(2)); // 2. index'den itibaren yazıyor
+        System.out.println(message.substring(2,7)); // 2. index ile 7. index arasında
+
+        System.out.println("---------");
+
+        for (String wrd: message.split(" ")){  // her bir bolukta satır atlıyor ve ayırıyor
+            System.out.println(wrd);
+        }
+
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+
 
 
 
